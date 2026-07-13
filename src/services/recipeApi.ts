84612c1f8392f48f3f3b839a5381c05b1b8fadc6
Ted Lim <tedlim@Teds-MacBook-Pro.local>
2026-07-13 09:14:19 -0400

    Add get detailed recipe api

--- src/services/recipeApi.ts
+++ src/services/recipeApi.ts
@@ -1,4 +1,4 @@
-import { api } from "./axios"; 
+import { api } from "./axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -12,6 +12,21 @@ export async function getRecipes(
     params: {
       ...(search && { search }),
     },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function getRecipe(
+  getToken: () => Promise<string | null>,
+  recipeId: string,
+) {
+  const token = await getToken();
+
+  const response = await api.get(`${API_URL}/recipes/${recipeId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
