df3c14bd3a7759e1c7cc1fe079aec78960676b5b
Ted Lim <tedlim@Teds-MacBook-Pro.local>
2026-07-24 16:51:16 -0400

    complete simple landing page with logo

--- src/layouts/PubilcLayout.tsx
+++ src/layouts/PubilcLayout.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@mui/material";
 import LandingPage from "../pages/LandingPage";
+import NavBar from "../components/ui/NavBar";
+import Login from "../components/auth/Login";
 
 type PublicLayoutProps = {
     children?: React.JSX.Element,
@@ -10,6 +12,9 @@ export default function PublicLayout({
 }: PublicLayoutProps) {
     return (
         <Container maxWidth="lg">
+            <NavBar>
+                <Login />
+            </NavBar>
             {children}
         </Container>
     )
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,35 @@
-import Login from "../components/auth/Login";
-import NavBar from "../components/ui/NavBar";
+// MUI
+import { Box, Typography } from "@mui/material";
 
+import logo from "../assets/logo.png";
 
 export default function LandingPage() {
     return (
-        <NavBar>
-            <Login />
-        </NavBar>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <Box
+            component={"img"}
+            src={logo}
+            sx={{
+                height: 256,
+                width: "auto",
+            }}
+            ></Box>
+            <Typography
+                variant="h2"
+                sx={{
+                    color: "primary",
+                    fontFamily: "Arial"
+                }}>
+                Your Personal Recipe Vault, Simplified.
+            </Typography>
+            <Typography>
+                Create, organize, and access all your favourite recipes in one distraction-free place.
+            </Typography>
+        </Box>
     )
 }
